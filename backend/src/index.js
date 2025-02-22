5dfbca57eb1f575331601fa2d56515775c556fbf
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-02-22 19:27:56 +0500

    more error fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,11 +28,8 @@ app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
 
-if (process.env.NODE_ENV === "development") {
-    server.listen(PORT, () => {
-        console.log("Server is running on port", PORT);
-        connectDb();
-    })
-}
-connectDb();
+server.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    connectDb();
+})
 export default app;
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: process.env.FRONTEND_URL,
-        methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+        methods: ["GET", "POST"],
         credentials: true
     },
     transports: ["websocket", "polling"] // Ensure polling is enabled
